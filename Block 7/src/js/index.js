2220d264890b96b2c9437932a8c46d43f3b63fbe
TaitoMi <gfdaker96@gmail.com>
2019-08-24 00:36:45 +0300

    some fixes

--- Block 7/src/js/index.js
+++ Block 7/src/js/index.js
@@ -73,3 +73,12 @@ if (window.innerWidth <= 668) {
 		},
 	});
 }
+
+/// ymaps
+ymaps.ready(init);
+function init(){ 
+	var myMap = new ymaps.Map("map", {
+			center: [55.76, 37.64],
+			zoom: 7
+	});
+}
