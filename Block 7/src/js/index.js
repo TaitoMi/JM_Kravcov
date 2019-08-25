fd9b28e9d84dbc9a967a3142b5814ad1e870a136
TaitoMi <gfdaker96@gmail.com>
2019-08-25 06:51:16 +0300

    finished

--- Block 7/src/js/index.js
+++ Block 7/src/js/index.js
@@ -5,10 +5,10 @@ const sidebar = document.querySelector('.sidebar');
 const searchResetBtn = document.querySelector('.search__reset');
 
 searchBtn.addEventListener('click', () => {
-	sidebar.classList.add('sidebar-active');
+	sidebar.classList.add('sidebar-input-active');
 })
 searchResetBtn.addEventListener('click', () => {
-	sidebar.classList.remove('sidebar-active');
+	sidebar.classList.remove('sidebar-input-active');
 })
 
 // Tabs
@@ -63,7 +63,61 @@ const repairContent = document.querySelector('.repair__items');
 
 linkMore(repairMore, 'repair__more', repairContent, 'repair__items');
 
-if (window.innerWidth <= 668) {
+
+const mobMenuBtn = document.querySelector('.mob-header__menu-btn');
+const mobSidebar = document.querySelector('.sidebar');
+const overlay = document.querySelector('.overlay');
+
+mobMenuBtn.addEventListener('click', () => {
+	mobSidebar.classList.toggle('sidebar-active');
+	mobMenuBtn.classList.toggle('mob-header__menu-btn-active');
+	overlay.classList.toggle('overlay-active');
+})
+
+function removeActive() {	
+	mobSidebar.classList.remove('sidebar-active');
+	mobMenuBtn.classList.remove('mob-header__menu-btn-active');
+	overlay.classList.remove('overlay-active');
+	document.querySelector('.m-feedback').classList.remove('m-feedback-active');
+	document.querySelector('.m-call').classList.remove('m-call-active');
+}
+
+overlay.addEventListener('click', () => {
+	removeActive();
+})
+
+function modalsShow(link, content, contentClassName) {
+	link.addEventListener('click', () => {
+		content.classList.toggle(`${contentClassName}-active`)
+		overlay.classList.toggle('overlay-active')
+	})
+}
+
+const modalCallLink = document.querySelectorAll('.modal-call');
+const modalCall = document.querySelector('.m-call');
+
+for(let i = 0; i < modalCallLink.length; i++) {
+	modalsShow(modalCallLink[i], modalCall, 'm-call');
+}
+
+const modalFeedbackLink = document.querySelectorAll('.modal-chat');
+const modalFeedback = document.querySelector('.m-feedback');
+
+for(let i = 0; i < modalFeedbackLink.length; i++) {
+	modalsShow(modalFeedbackLink[i], modalFeedback, 'm-feedback');
+}
+
+const modalCloseBtn = document.querySelectorAll('.modal__close-btn');
+
+for (let i = 0; i < modalCloseBtn.length; i++) {
+	const el = modalCloseBtn[i];
+	el.addEventListener('click', () => {
+		removeActive();
+	})
+}
+
+
+if (window.innerWidth <= 600) {
 	var swiper = new Swiper('.swiper-container', {
 		slidesPerView: 'auto',
 		spaceBetween: 30,
@@ -74,6 +128,8 @@ if (window.innerWidth <= 668) {
 	});
 }
 
+
+
 /// ymaps
 ymaps.ready(init);
 function init(){ 
@@ -81,4 +137,11 @@ function init(){
 			center: [55.76, 37.64],
 			zoom: 7
 	});
+	myMap.controls.remove('geolocationControl');
+	myMap.controls.remove('searchControl');
+	myMap.controls.remove('trafficControl');
+	myMap.controls.remove('typeSelector');
+	myMap.controls.remove('fullscreenControl');
+	myMap.controls.remove('rulerControl');
+	myMap.behaviors.disable(['scrollZoom']);
 }
